84fe4b6abfff66159632975053631995e731f61d
Adam DePaolo <amdepaolo@gmail.com>
2023-04-15 17:45:09 -0400

    commit tree 3d1d00ec3ed90e091c80356e10758a515cece0fb

--- src/features/cats/CatList.js
+++ src/features/cats/CatList.js
@@ -1,1 +1,13 @@
-// write your CatList component here
+import React from "react";
+
+export default function CatList({catPics}){
+
+    const catPicList = catPics.map(catPic => <img src={catPic.url} key={catPic.id} alt="cat" />
+    )
+
+    return(
+        <div>
+            {catPicList}
+        </div>
+    )
+}
